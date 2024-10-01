3a1bb7e00806186ad53a2cb3d0bc5af876dd3627
pparksso <thgml5143@hanmail.net>
2024-10-01 22:51:18 +0900

    feat: home component

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
-      <Text>Home screen</Text>
+      <Text>CheerUp Time</Text>
+      <View>
+        <Pressable onPress={() => navigation.navigate("Setting")}>
+          <Text>응원 설정</Text>
+        </Pressable>
+        <Pressable onPress={() => navigation.navigate("List")}>
+          <Text>응원 목록</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {},
+  title: {},
+  button: {},
+  buttonText: {},
+});
