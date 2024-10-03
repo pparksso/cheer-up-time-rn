06b1c83ec6cb0f941a76861f6daa3a2e83f87f35
pparksso <thgml5143@hanmail.net>
2024-10-03 13:38:29 +0900

    design: Home

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
+import { SafeAreaView, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
 import HomeScreen from "./screens/HomeScreen";
 import MessageScreen from "./screens/MessageScreen";
 import AlarmListScreen from "./screens/AlarmListScreen";
 import AlarmSettingScreen from "./screens/AlarmSettingScreen";
-import { SafeAreaView, StyleSheet } from "react-native";
 
 export default function App() {
   const stack = createNativeStackNavigator();
+
+  const [fontsLoaded] = useFonts({
+    parisienne: require("./assets/fonts/Parisienne-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) return null;
   return (
     <>
       <StatusBar style="auto" />
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,15 +1,23 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet, Image } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>CheerUp Time</Text>
-      <View>
-        <Pressable onPress={() => navigation.navigate("Setting")}>
-          <Text>응원 설정</Text>
+    <View style={styles.container}>
+      <View style={styles.titleWrap}>
+        <Text style={styles.title}>CheerUp Time</Text>
+      </View>
+      <View style={styles.buttonWrap}>
+        <Pressable
+          onPress={() => navigation.navigate("Setting")}
+          style={[styles.button, { marginBottom: 30 }]}
+        >
+          <Text style={styles.buttonText}>응원 설정</Text>
         </Pressable>
-        <Pressable onPress={() => navigation.navigate("List")}>
-          <Text>응원 목록</Text>
+        <Pressable
+          onPress={() => navigation.navigate("List")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>응원 목록</Text>
         </Pressable>
       </View>
     </View>
@@ -19,8 +27,34 @@ const HomeScreen = ({ navigation }) => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {},
-  title: {},
-  button: {},
-  buttonText: {},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  titleWrap: {
+    height: "70%",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 50,
+    fontFamily: "parisienne",
+  },
+  buttonWrap: {
+    width: "100%",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "rgb(254, 198, 96)",
+    width: "70%",
+    alignItems: "center",
+    paddingVertical: 15,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#333",
+    letterSpacing: 2,
+  },
 });
